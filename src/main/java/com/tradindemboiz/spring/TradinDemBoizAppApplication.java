b975c00743d993b0f406b66576fdfda2b76de88e
smartywebdev0119 <smartwebdev0119@gmail.com>
2020-11-19 18:04:15 +0100

    Created a spring application with neede dependencies

---
+++ src/main/java/com/tradindemboiz/spring/TradinDemBoizAppApplication.java
@@ -0,0 +1,13 @@
+package com.tradindemboiz.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradinDemBoizAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradinDemBoizAppApplication.class, args);
+	}
+
+}
