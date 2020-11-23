1d54df3cf6316845afdb3f259856d07327c7e035
smartywebdev0119 <smartwebdev0119@gmail.com>
2020-11-23 09:55:50 +0100

    Moved some files

--- src/main/java/com/tradindemboiz/spring/config/MyWebMvcConfigurer.java
+++ src/main/java/com/tradindemboiz/spring/config/MyWebMvcConfigurer.java
@@ -28,7 +28,7 @@ class MyWebMvcConfigurer implements WebMvcConfigurer {
                                          Resource location) throws IOException {
             Resource requestedResource = location.createRelative(resourcePath);
             return requestedResource.exists() && requestedResource.isReadable() ? requestedResource
-                : new ClassPathResource("/static/index.html");
+                : new ClassPathResource("/static/index.html"); //TODO Fråga Johan vad som ska stå här?..
           }
         });
   }
