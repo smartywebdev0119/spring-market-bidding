7d0c237815986aa6579cc23676ab33a75888cc2f
smartywebdev0119 <smartwebdev0119@gmail.com>
2020-11-23 17:15:33 +0100

    Added timer functions

--- src/main/resources/frontend/src/components/AuctionTimer.vue
+++ src/main/resources/frontend/src/components/AuctionTimer.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="auction-details">
-    <h3 class="title">Time left:</h3>
+    <h3 class="title">Expires:</h3>
     <span class="details-value detail-time-left">{{ timeLeft }}</span>
   </div>
 </template>
@@ -12,16 +12,61 @@ import { Vue, Component, Prop } from "vue-property-decorator";
 export default class AuctionTimer extends Vue {
   @Prop({type: Number, required: true})
   endDate
+  timer = null;
+  countdown = 0;
 
   get timeLeft() {
-    const date = new Date(this.endDate);
-    return date.toDateString();
+    if (this.countdown <= 0) {
+      return "Expired";
+    }
+    return this.getReadableCountdown(this.countdown);
+  }
+
+  getReadableCountdown(countdown) {
+    const timeObject = this.getTimeObject(countdown);
+    if(timeObject.weeks > 0) {
+      return new Date(this.endDate).toDateString();
+    } else if (timeObject.days > 0) {
+      return `${timeObject.days} days`;
+    } else if (timeObject.hours > 1) {
+      return `${timeObject.hours} h ${timeObject.minutes} min`;
+    } else if (timeObject.hours > 0) {
+      return `${timeObject.hours} h ${timeObject.minutes} min ${timeObject.seconds} sec`;
+    } else if (timeObject.minutes > 0) {
+      return `${timeObject.minutes} min ${timeObject.seconds} sec`;
+    } else {
+      return `${timeObject.seconds} sec`;
+    }
+  }
+
+  getTimeObject(countdown) {
+    const secondInMs = 1000;
+    const minuteInMs = secondInMs * 60;
+    const hourInMs = minuteInMs * 60;
+    const dayInMs = hourInMs * 24;
+    const weekInMs = dayInMs * 7;
+    const timeObject = {}
+    timeObject.weeks = Math.floor(countdown / weekInMs)
+    countdown -= timeObject.weeks * weekInMs;
+    timeObject.days = Math.floor(countdown / dayInMs)
+    countdown -= timeObject.days * dayInMs;
+    timeObject.hours = Math.floor(countdown / hourInMs)
+    countdown -= timeObject.hours * hourInMs;
+    timeObject.minutes = Math.floor(countdown / minuteInMs)
+    countdown -= timeObject.minutes * minuteInMs;
+    timeObject.seconds = Math.floor(countdown / secondInMs)
+    return timeObject;
   }
 
   created() {
+    this.countdown = this.endDate - Date.now();
+    this.timer = setInterval(() => {
+      this.countdown -= 1000
+    }, 1000)
   }
 
   destroyed() {
+    clearInterval(this.timer);
   }
 }
 </script>
