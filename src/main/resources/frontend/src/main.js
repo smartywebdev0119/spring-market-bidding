4e05f5f791c5a47cc8e07fe7ef22a066ea8278b9
smartywebdev0119 <smartwebdev0119@gmail.com>
2020-11-23 16:39:13 +0100

    Bootstrap override example + custom color tag

--- src/main/resources/frontend/src/main.js
+++ src/main/resources/frontend/src/main.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import "../node_modules/bootstrap/dist/css/bootstrap.css"
+import "./assets/_custom.scss"
 
 Vue.config.productionTip = false
 
