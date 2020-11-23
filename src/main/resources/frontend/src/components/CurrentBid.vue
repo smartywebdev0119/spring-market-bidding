2ec05ce8a4da679013f039fa921ae7c9431c7df3
smartywebdev0119 <smartwebdev0119@gmail.com>
2020-11-23 15:53:28 +0100

    Added basic current bid component

---
+++ src/main/resources/frontend/src/components/CurrentBid.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="auction-details">
+    <h3 class="title">Highest bid:</h3>
+    <span class="details-value detail-bid">{{ currentBid }} SEK</span>
+  </div>
+</template>
+
+<script>
+import { Vue, Component, Prop } from "vue-property-decorator";
+
+@Component
+export default class CurrentBid extends Vue {
+  @Prop({type: Number, required: true})
+  currentBid
+}
+</script>
+
+<style lang="scss" scoped>
+  .auction-details {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    .title {
+      color: #288781;
+      font-style: italic;
+      font-size: 18px;
+    }
+  }
+</style>
