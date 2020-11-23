06bf3c22f7bb5cbe17d4450915e9a32ba4d60fba
smartywebdev0119 <smartwebdev0119@gmail.com>
2020-11-23 14:43:57 +0100

    Added Search.vue

---
+++ src/main/resources/frontend/src/components/home-page/Search.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="search-wrapper">
+    <form >
+      <input id="search-field" type="text">
+      <button id="search">search</button>
+    </form>
+  </div>
+</template>
+
+<script>
+import { Vue, Component } from "vue-property-decorator";
+
+@Component
+export default class Search extends Vue {
+
+}
+</script>
+
+<style lang="scss" scoped></style>
--- src/main/resources/frontend/src/views/Home.vue
+++ src/main/resources/frontend/src/views/Home.vue
@@ -1,13 +1,19 @@
 <template>
   <div class="home-page">
     <h1>This is the home page</h1>
+    <Search />
   </div>
 </template>
 
 <script>
 import { Component, Vue } from "vue-property-decorator";
+import Search from "../components/home-page/Search";
 
-@Component
+@Component({
+  components: {
+    Search,
+  },
+})
 export default class Home extends Vue {}
 </script>
 
