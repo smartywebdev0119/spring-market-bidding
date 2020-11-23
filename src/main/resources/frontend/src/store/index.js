1bbef3971b4825d1a71768cd27a726efcc21af59
smartywebdev0119 <smartwebdev0119@gmail.com>
2020-11-23 15:54:30 +0100

    Added auction logic to store

--- src/main/resources/frontend/src/store/index.js
+++ src/main/resources/frontend/src/store/index.js
@@ -1,15 +1,23 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    auction: null,
   },
   mutations: {
+    setAuction(state, value) {
+      state.auction = value;
+    },
   },
   actions: {
+    async fetchAuction({ commit }, id) {
+      const raw = await fetch(`/api/v1/auctions/${id}`);
+      const auction = await raw.json();
+      commit("setAuction", auction);
+    },
   },
-  modules: {
-  }
-})
+  modules: {},
+});
