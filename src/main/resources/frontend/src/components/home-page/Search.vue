1d7e1e19935672ea10811ae15ec46f0c74542d73
smartywebdev0119 <smartwebdev0119@gmail.com>
2020-11-23 15:08:02 +0100

    Logic in searchComponent is done, you get back results according to the search query

--- src/main/resources/frontend/src/components/home-page/Search.vue
+++ src/main/resources/frontend/src/components/home-page/Search.vue
@@ -1,18 +1,27 @@
 <template>
   <div class="search-wrapper">
-    <form >
-      <input id="search-field" type="text">
+    <form @submit.prevent="executeSearch">
+      <input id="search-field" type="text" v-model="searchQuery" />
       <button id="search">search</button>
     </form>
   </div>
 </template>
 
 <script>
-import { Vue, Component } from "vue-property-decorator";
+import { Vue, Component, Watch } from "vue-property-decorator";
 
 @Component
 export default class Search extends Vue {
+  searchQuery = "";
+  executeSearch() {
+    this.$store.dispatch("fetchAllAuctionsWithQuery", this.searchQuery);
+  }
 
+  //TODO Remove this watch later on.
+  @Watch("$store.state.auctions")
+  onAuctionsChange(newVal) {
+    console.log(newVal);
+  }
 }
 </script>
 
--- src/main/resources/frontend/src/store/index.js
+++ src/main/resources/frontend/src/store/index.js
@@ -1,15 +1,25 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    auctions: null,
   },
   mutations: {
+    updateAuctions(state, data) {
+      state.auctions = data;
+    },
   },
   actions: {
+    async fetchAllAuctionsWithQuery({ commit }, searchQuery) {
+      let auctionResults = await fetch(
+        `/api/v1/auctions?search=${searchQuery}`
+      );
+      auctionResults = await auctionResults.json();
+      commit("updateAuctions", auctionResults);
+    },
   },
-  modules: {
-  }
-})
+  modules: {},
+});
