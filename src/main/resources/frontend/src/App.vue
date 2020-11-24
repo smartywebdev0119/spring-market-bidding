5a51b44d3402d34aac1f11213de6adeea4af45e9
smartywebdev0119 <smartwebdev0119@gmail.com>
2020-11-24 12:01:50 +0100

    NavBar css and imports/exports

--- src/main/resources/frontend/src/App.vue
+++ src/main/resources/frontend/src/App.vue
@@ -1,12 +1,16 @@
 <template>
   <div id="app">
+    <NavBar/>
     <router-view/>
   </div>
 </template>
 
 <script>
+import NavBar  from '../src/components/NavBar'
+
+
 export default {
-  
+  components:{NavBar}
 }
 </script>
 
---
+++ src/main/resources/frontend/src/components/NavBar.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="navbar">
+        <h1 class="title">D3mTr@din'Bo!z</h1>
+    
+        <i class="material-icons">menu</i>
+    
+    </div>
+
+</template>
+
+<script>
+
+import { Vue, Component } from "vue-property-decorator";
+@Component
+export default class NavBar extends Vue {
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+.navbar{
+    display: grid;
+    align-items: center;
+    grid-template-columns: 1fr 3fr 1fr;
+    column-gap: 5px;
+    background: rosybrown;
+    
+    .title{
+        grid-column: 2/3;
+        justify-self: center;
+        margin: 0;
+        font-size: 1.6em;
+    }
+   
+    i{
+        font-size: 2em;
+        color: #288781;
+        cursor: pointer;
+        justify-self: center;
+    }
+}
+ 
+</style>
