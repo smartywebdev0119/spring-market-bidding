b93b78c402684032f59dd0e0ee379546bff59346
smartywebdev0119 <smartwebdev0119@gmail.com>
2020-11-25 14:14:09 +0100

    Fixed back button positioning

--- src/main/resources/frontend/src/views/Auction.vue
+++ src/main/resources/frontend/src/views/Auction.vue
@@ -1,20 +1,22 @@
 <template>
   <div v-if="auction" class="detailed-auction container">
     <div class="auction-header row">
-      <button @click="goBack" class="back-arrow col-2">
-        <svg
-          height="25px"
-          viewBox="0 0 16 16"
-          class="bi bi-arrow-left"
-          fill="#288781"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            fill-rule="evenodd"
-            d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z"
-          />
-        </svg>
-      </button>
+      <div class="back-arrow col-2">
+        <button @click="goBack">
+          <svg
+            height="25px"
+            viewBox="0 0 16 16"
+            class="bi bi-arrow-left"
+            fill="#288781"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z"
+            />
+          </svg>
+        </button>
+      </div>
       <h2 class="auction-title col-10">
         {{ auction.title }}
       </h2>
@@ -102,12 +104,16 @@ export default class Auction extends Vue {
   height: 40px;
   margin-bottom: 20px;
   .back-arrow {
-    border: none;
-    background: none;
-    &:focus {
-      outline: none;
-      svg {
-        border: 1px solid #288781;
+    display: flex;
+    justify-content: flex-start;
+    button {
+      border: none;
+      background: none;
+      &:focus {
+        outline: none;
+        svg {
+          border: 1px solid #288781;
+        }
       }
     }
   }
