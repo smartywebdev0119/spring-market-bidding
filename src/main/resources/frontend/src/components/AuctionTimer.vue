7e9e399e1b45df32242c614ed5e3d64da9b181aa
smartywebdev0119 <smartwebdev0119@gmail.com>
2020-11-25 15:30:30 +0100

    Added another timer output

--- src/main/resources/frontend/src/components/AuctionTimer.vue
+++ src/main/resources/frontend/src/components/AuctionTimer.vue
@@ -10,13 +10,15 @@ import { Vue, Component, Prop } from "vue-property-decorator";
 
 @Component
 export default class AuctionTimer extends Vue {
-  @Prop({type: Number, required: true})
-  endDate
+  @Prop({ type: Number, required: true })
+  endDate;
   timer = null;
   countdown = 0;
 
   get timeLeft() {
-    return this.countdown <= 0 ? "Expired" : this.getReadableCountdown(this.countdown);
+    return this.countdown <= 0
+      ? "Expired"
+      : this.getReadableCountdown(this.countdown);
   }
 
   get isUrgent() {
@@ -25,10 +27,12 @@ export default class AuctionTimer extends Vue {
 
   getReadableCountdown(countdown) {
     const timeObject = this.getTimeObject(countdown);
-    if(timeObject.weeks > 0) {
+    if (timeObject.weeks > 0) {
       return new Date(this.endDate).toDateString();
-    } else if (timeObject.days > 0) {
+    } else if (timeObject.days > 1) {
       return `${timeObject.days} days`;
+    } else if (timeObject.days > 0) {
+      return `${timeObject.days} day ${timeObject.hours} h`;
     } else if (timeObject.hours > 1) {
       return `${timeObject.hours} h ${timeObject.minutes} min`;
     } else if (timeObject.hours > 0) {
@@ -46,24 +50,24 @@ export default class AuctionTimer extends Vue {
     const hourInMs = minuteInMs * 60;
     const dayInMs = hourInMs * 24;
     const weekInMs = dayInMs * 7;
-    const timeObject = {}
-    timeObject.weeks = Math.floor(countdown / weekInMs)
+    const timeObject = {};
+    timeObject.weeks = Math.floor(countdown / weekInMs);
     countdown -= timeObject.weeks * weekInMs;
-    timeObject.days = Math.floor(countdown / dayInMs)
+    timeObject.days = Math.floor(countdown / dayInMs);
     countdown -= timeObject.days * dayInMs;
-    timeObject.hours = Math.floor(countdown / hourInMs)
+    timeObject.hours = Math.floor(countdown / hourInMs);
     countdown -= timeObject.hours * hourInMs;
-    timeObject.minutes = Math.floor(countdown / minuteInMs)
+    timeObject.minutes = Math.floor(countdown / minuteInMs);
     countdown -= timeObject.minutes * minuteInMs;
-    timeObject.seconds = Math.floor(countdown / secondInMs)
+    timeObject.seconds = Math.floor(countdown / secondInMs);
     return timeObject;
   }
 
   created() {
     this.countdown = this.endDate - Date.now();
     this.timer = setInterval(() => {
-      this.countdown -= 1000
-    }, 1000)
+      this.countdown -= 1000;
+    }, 1000);
   }
 
   destroyed() {
@@ -73,21 +77,21 @@ export default class AuctionTimer extends Vue {
 </script>
 
 <style lang="scss" scoped>
-  .auction-details {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    .title {
-      color: #288781;
-      font-style: italic;
-      font-size: 18px;
-    }
-    .time-left {
-      font-size: 18px;
-    }
+.auction-details {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  .title {
+    color: #288781;
+    font-style: italic;
+    font-size: 18px;
   }
-  .urgent {
-    font-weight: bold;
-    color: red;
+  .time-left {
+    font-size: 18px;
   }
+}
+.urgent {
+  font-weight: bold;
+  color: red;
+}
 </style>
