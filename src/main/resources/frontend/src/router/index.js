cdb8c6e9824da20e4453be90e4dd628596713150
smartywebdev0119 <smartwebdev0119@gmail.com>
2020-11-26 10:48:28 +0100

    added new page and route to page

--- src/main/resources/frontend/src/router/index.js
+++ src/main/resources/frontend/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
 import Auction from "../views/Auction.vue";
+import CreateAuction from "../views/CreateAuction.vue";
 
 Vue.use(VueRouter);
 
@@ -15,6 +16,11 @@ const routes = [
     path: "/auction/:id",
     name: "Auction",
     component: Auction,
+  },
+  {
+    path: "/create-auction",
+    name: "CreateAuction",
+    component: CreateAuction,
   },
 ];
 
---
+++ src/main/resources/frontend/src/views/CreateAuction.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="container">
+    <div class="text-center header-text">Create Auction</div>
+    <div>
+        <p>Title</p>
+        <input type="text" value="" required>
+    </div>
+    <div>
+        <p>Starting price</p>
+        <input/>
+    </div>
+    <div>
+        <p>Auction end date</p>
+        <input/>
+    </div>
+    <div>
+        <p>Image url</p>
+        <input/>
+    </div>
+    <div>
+        <p>Description</p>
+        <input/>
+    </div>
+    <button type="button" class="btn btn-primary">>Create auction</button>
+    
+  </div>
+</template>
+
+<script>
+import { Component, Vue } from "vue-property-decorator";
+
+@Component({
+  components: {
+    
+  },
+})
+export default class Home extends Vue {
+
+    newAuction = {
+        title: null,
+        description: null,
+        start_price: null,
+        end_date: null,
+        image_URL: null,
+        timestamp: null
+    };
+
+  created(){}
+    
+}
+</script>
+
+<style lang="scss" scoped>
+.header-text{
+    font-size: 2em;
+    color: #288781;
+}
+
+</style>
